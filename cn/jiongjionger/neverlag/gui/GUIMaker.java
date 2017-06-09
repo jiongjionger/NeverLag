15eabb27cbc24bb0333f0de90f0731fa7e5a00f3
jiongjionger <17jiong@gmail.com>
2017-06-09 23:10:01 +0800

    实现底层GUI类

---
+++ cn/jiongjionger/neverlag/gui/GUIMaker.java
@@ -0,0 +1,123 @@
+package cn.jiongjionger.neverlag.gui;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+import org.bukkit.inventory.ItemStack;
+
+public class GUIMaker {
+
+	private Inventory inv;
+
+	public GUIMaker(Inventory inv) {
+		this.inv = Bukkit.createInventory(inv.getHolder(), inv.getSize(), inv.getTitle());
+		this.inv.setContents(inv.getContents());
+	}
+
+	public GUIMaker(int size, String title) {
+		this.inv = Bukkit.createInventory(null, size, title);
+	}
+
+	/*
+	 * public GUIMaker(InventoryType type, String title) { this.inv =
+	 * Bukkit.createInventory(null, type, title); }
+	 * 
+	 * public GUIMaker(InventoryHolder owner, InventoryType type, String title)
+	 * { this.inv = Bukkit.createInventory(owner, type, title); }
+	 */
+
+	public GUIMaker(InventoryHolder owner, int size, String title) {
+		this.inv = Bukkit.createInventory(owner, size, title);
+	}
+
+	public GUIMaker(InventoryHolder owner, int size) {
+		this.inv = Bukkit.createInventory(owner, size);
+	}
+
+	// 填充物品
+	public GUIMaker fillItem(ItemStack item, int x, int y) {
+		int slot = (y - 1) * 9 + x - 1;
+		if (slot > this.inv.getSize() - 1) {
+			return this;
+		}
+		this.inv.setItem(slot, item);
+		return this;
+	}
+
+	public GUIMaker fillItem(ItemStack item, int[]... pos) {
+		for (int[] nowpos : pos) {
+			if (nowpos.length == 2) {
+				int slot = (nowpos[1] - 1) * 9 + nowpos[0] - 1;
+				if (slot > this.inv.getSize() - 1) {
+					return this;
+				}
+				this.inv.setItem(slot, item);
+			}
+		}
+		return this;
+	}
+
+	public GUIMaker fillItem(ItemStack item, int slot) {
+		if (slot > this.inv.getSize() - 1) {
+
+			return this;
+		}
+		this.inv.setItem(slot, item);
+		return this;
+	}
+
+	// 删除物品
+	public GUIMaker removeItem(int x, int y) {
+		int slot = (y - 1) * 9 + x - 1;
+		this.inv.clear(slot);
+		return this;
+	}
+
+	// 清空物品
+	public GUIMaker clearItem() {
+		this.inv.clear();
+		return this;
+	}
+
+	// 填充空白物品
+	public GUIMaker fillBlank(ItemStack item) {
+		for (int i = 0; i < this.inv.getSize(); i++) {
+			if (this.inv.getItem(i) == null || this.inv.getItem(i).getType().equals(Material.AIR)) {
+				this.inv.setItem(i, item);
+			}
+		}
+		return this;
+	}
+
+	// 填充间隔物品
+	public GUIMaker fillWhite(ItemStack item) {
+		for (int i = 0; i < this.inv.getSize(); i++) {
+			if (this.inv.getItem(i) != null && !this.inv.getItem(i).getType().equals(Material.AIR) && !this.inv.getItem(i).isSimilar(item)) {
+				int left_slot = i - 1;
+				int right_slot = i + 1;
+				if (getRow(i) == getRow(left_slot) && left_slot >= 0) {
+					if (this.inv.getItem(left_slot) == null || this.inv.getItem(left_slot).getType().equals(Material.AIR)) {
+						this.inv.setItem(left_slot, item);
+					}
+				}
+				if (getRow(i) == getRow(right_slot) && right_slot <= this.inv.getSize() - 1) {
+					if (this.inv.getItem(right_slot) == null || this.inv.getItem(right_slot).getType().equals(Material.AIR)) {
+						this.inv.setItem(right_slot, item);
+					}
+				}
+			}
+		}
+		return this;
+	}
+
+	// 获取
+	public Inventory get() {
+		return this.inv;
+	}
+
+	// 获取行数
+	public int getRow(int slot) {
+		return slot / 9 + 1;
+	}
+}
---
+++ cn/jiongjionger/neverlag/gui/IconMaker.java
@@ -0,0 +1,98 @@
+package cn.jiongjionger.neverlag.gui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class IconMaker {
+	private ItemStack item;
+
+	public IconMaker(String type) {
+		try {
+			Material m = Material.valueOf(type);
+			this.item = new ItemStack(m);
+		} catch (Exception e) {
+			this.item = new ItemStack(Material.STONE);
+		}
+	}
+
+	public IconMaker(Material type) {
+		this.item = new ItemStack(type);
+	}
+
+	public IconMaker(ItemStack item) {
+		this.item = item.clone();
+	}
+
+	// 设置显示名称
+	public IconMaker setDisplayName(String name) {
+		ItemMeta m = this.item.getItemMeta();
+		m.setDisplayName(ChatColor.translateAlternateColorCodes('&', name));
+		this.item.setItemMeta(m);
+		return this;
+	}
+
+	// 设置Lore
+	public IconMaker setLore(List<String> lore) {
+		List<String> colorLore = new ArrayList<String>();
+		for (String l : lore) {
+			colorLore.add(ChatColor.translateAlternateColorCodes('&', l));
+		}
+		ItemMeta m = this.item.getItemMeta();
+		m.setLore(colorLore);
+		this.item.setItemMeta(m);
+		return this;
+	}
+
+	// 设置Lore
+	public IconMaker setLore(String... lore) {
+		List<String> colorLore = new ArrayList<String>();
+		for (String l : lore) {
+			colorLore.add(ChatColor.translateAlternateColorCodes('&', l));
+		}
+		ItemMeta m = this.item.getItemMeta();
+		m.setLore(colorLore);
+		this.item.setItemMeta(m);
+		return this;
+	}
+
+	// 设置耐久度，羊毛等通过这个设置颜色
+	public IconMaker setDurability(short d) {
+		this.item.setDurability(d);
+		return this;
+	}
+
+	// 设置数量
+	public IconMaker setAmount(int amount) {
+		this.item.setAmount(amount);
+		return this;
+	}
+
+	// 替换Lore的内容
+	public IconMaker replaceLore(String oldlore, String newlore) {
+		if (this.item != null && this.item.hasItemMeta() && this.item.getItemMeta().hasLore()) {
+			oldlore = ChatColor.translateAlternateColorCodes('&', oldlore);
+			newlore = ChatColor.translateAlternateColorCodes('&', newlore);
+
+			List<String> colorLore = new ArrayList<String>();
+			for (String l : this.item.getItemMeta().getLore()) {
+				colorLore.add(l.replace(oldlore, newlore));
+			}
+
+			ItemMeta m = this.item.getItemMeta();
+			m.setLore(colorLore);
+			this.item.setItemMeta(m);
+			return this;
+		}
+		return this;
+	}
+
+	// 获取Item
+	public ItemStack getItem() {
+		return this.item;
+	}
+}
