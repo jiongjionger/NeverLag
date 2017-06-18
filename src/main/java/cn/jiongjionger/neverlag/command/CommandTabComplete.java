23f3c8f1c429e0342940893e62057b54f903d394
jiongjionger <17jiong@gmail.com>
2017-06-18 12:23:54 +0800

    性能分析实现自动补全插件名称

--- src/main/java/cn/jiongjionger/neverlag/command/CommandTabComplete.java
+++ src/main/java/cn/jiongjionger/neverlag/command/CommandTabComplete.java
@@ -3,12 +3,16 @@ package cn.jiongjionger.neverlag.command;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.EntityType;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.util.StringUtil;
 
 public class CommandTabComplete implements TabCompleter {
@@ -18,6 +22,7 @@ public class CommandTabComplete implements TabCompleter {
 	private static final String[] CHUNKINFO_TYPE = { "entity", "tiles", "monsters", "animals", "dropitem", "player", "villager", "squid", "chest", "hopper",
 			"furnace", "dropper", "dispenser", "piston", "noteblock", "jukebox", "brewing", "cauldron", "armorstand", "skull" };
 	private static final String[] TIMINGS = { "on", "off", "lag", "event", "command", "task", "plugin" };
+	private static final Set<String> SHOULD_COMPLETE_PLUGIN_NAME = new HashSet<String>();
 
 	static {
 		for (EntityType type : EntityType.values()) {
@@ -30,6 +35,10 @@ public class CommandTabComplete implements TabCompleter {
 		CLEARTYPE.add("dropitem");
 		CLEARTYPE.add("animals");
 		CLEARTYPE.add("monsters");
+		SHOULD_COMPLETE_PLUGIN_NAME.add("event");
+		SHOULD_COMPLETE_PLUGIN_NAME.add("command");
+		SHOULD_COMPLETE_PLUGIN_NAME.add("task");
+		SHOULD_COMPLETE_PLUGIN_NAME.add("plugin");
 	}
 
 	@Override
@@ -56,8 +65,26 @@ public class CommandTabComplete implements TabCompleter {
 				StringUtil.copyPartialMatches(args[1], Arrays.asList(TIMINGS), completions);
 				break;
 			}
+		case 3:
+			if (args[0].equalsIgnoreCase("timings") && SHOULD_COMPLETE_PLUGIN_NAME.contains(args[1].toLowerCase())) {
+				StringUtil.copyPartialMatches(args[2], getAllPluginName(), completions);
+				break;
+			}
+		default:
+			break;
 		}
+
 		Collections.sort(completions);
 		return completions;
+	}
+
+	private List<String> getAllPluginName() {
+		List<String> pluginNameList = new ArrayList<String>();
+		for (Plugin plugin : Bukkit.getPluginManager().getPlugins()) {
+			if (plugin != null && plugin.isEnabled()) {
+				pluginNameList.add(plugin.getName());
+			}
+		}
+		return pluginNameList;
 	}
 }
