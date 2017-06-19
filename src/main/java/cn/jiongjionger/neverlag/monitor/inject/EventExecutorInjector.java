bf5d09a2ff2a1c4d330b0744c33ff75e3613383d
jiongjionger <17jiong@gmail.com>
2017-06-19 20:33:28 +0800

    未来不再依赖从TimedRegisteredListener获取timings的统计信息

--- src/main/java/cn/jiongjionger/neverlag/monitor/inject/EventExecutorInjector.java
+++ src/main/java/cn/jiongjionger/neverlag/monitor/inject/EventExecutorInjector.java
@@ -7,23 +7,21 @@ import org.bukkit.event.Listener;
 import org.bukkit.plugin.EventExecutor;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredListener;
-import org.bukkit.plugin.TimedRegisteredListener;
 
 import cn.jiongjionger.neverlag.utils.Reflection;
 import cn.jiongjionger.neverlag.utils.Reflection.FieldAccessor;
 
 public class EventExecutorInjector extends AbstractMultipleInjector implements EventExecutor {
+
 	// 将监听器原本的EventExecutor替换成带性能统计的版本
 	public static void inject(Plugin plg) {
 		if (plg != null) {
 			for (RegisteredListener listener : HandlerList.getRegisteredListeners(plg)) {
 				try {
-					if (!(listener instanceof TimedRegisteredListener)) {
-						HandlerList.unregisterAll(listener.getListener());
-						FieldAccessor<EventExecutor> field = Reflection.getField(RegisteredListener.class, "executor", EventExecutor.class);
-						EventExecutor fieldEventExecutor = field.get(listener);
-						field.set(listener, new EventExecutorInjector(plg, fieldEventExecutor));
-					}
+					HandlerList.unregisterAll(listener.getListener());
+					FieldAccessor<EventExecutor> field = Reflection.getField(RegisteredListener.class, "executor", EventExecutor.class);
+					EventExecutor fieldEventExecutor = field.get(listener);
+					field.set(listener, new EventExecutorInjector(plg, fieldEventExecutor));
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -36,13 +34,11 @@ public class EventExecutorInjector extends AbstractMultipleInjector implements E
 		if (plg != null) {
 			for (RegisteredListener listener : HandlerList.getRegisteredListeners(plg)) {
 				try {
-					if (!(listener instanceof TimedRegisteredListener)) {
-						FieldAccessor<EventExecutor> field = Reflection.getField(RegisteredListener.class, "executor", EventExecutor.class);
-						EventExecutor executor = field.get(listener);
-						if (executor instanceof EventExecutorInjector) {
-							HandlerList.unregisterAll(listener.getListener());
-							field.set(listener, ((EventExecutorInjector) executor).getEventExecutor());
-						}
+					FieldAccessor<EventExecutor> field = Reflection.getField(RegisteredListener.class, "executor", EventExecutor.class);
+					EventExecutor executor = field.get(listener);
+					if (executor instanceof EventExecutorInjector) {
+						HandlerList.unregisterAll(listener.getListener());
+						field.set(listener, ((EventExecutorInjector) executor).getEventExecutor());
 					}
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -50,7 +46,7 @@ public class EventExecutorInjector extends AbstractMultipleInjector implements E
 			}
 		}
 	}
-	
+
 	private final EventExecutor eventExecutor;
 
 	public EventExecutorInjector(Plugin plugin, EventExecutor eventExecutor) {
@@ -65,9 +61,9 @@ public class EventExecutorInjector extends AbstractMultipleInjector implements E
 			this.eventExecutor.execute(listener, e);
 		} else {
 			long startTime = System.nanoTime();
-			try{
+			try {
 				this.eventExecutor.execute(listener, e);
-			}finally{
+			} finally {
 				long endTime = System.nanoTime();
 				long executeTime = endTime - startTime;
 				this.record(e.getEventName(), executeTime);
